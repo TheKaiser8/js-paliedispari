accf36768df8f6cf04fbbd6ab0d16fdd265df3d3
TheKaiser8 <stefano.vanzini8@gmail.com>
2022-10-28 17:17:20 +0200

    Aggiunge funzione  palindromo Il meccanismo funziona ma è da sistemare, creati come test 2 console.log

--- palindroma/main.js
+++ palindroma/main.js
@@ -9,3 +9,42 @@
 /*------------------
     MAIN
 --------------------*/
+const word = prompt("Inserisci una parola");
+console.log(word);
+let wordSpelling;
+let wordSpellingReverse;
+
+
+function palindromes(word) {
+
+    for (let i = 0; i < word.length; i++) {
+        console.log(word[i]);
+        wordSpelling = word[i];
+    }
+    for (let i = word.length; i >= 0; i--) {
+        console.log(word[i]);
+        wordSpellingReverse = word[i];
+    }
+    if( wordSpelling === wordSpellingReverse ) {
+        return true;
+    }
+    return false;
+}
+
+// Test
+console.log('Inserisco la parola "osso" e mi aspetto true', palindromes(word));
+console.log('Inserisco la parola "ciao" e mi aspetto false', palindromes(word));
+
+
+
+// const palindrome = word;
+// function palindromes (word) {
+//     for (let i = word.length; i >= 0; i--) {
+//         if( palindrome === word ) {
+//             console.log("La parola è palindroma");
+//             return true;
+//         }
+//     }
+//     return false;
+// }
+// console.log(palindromes(word));
