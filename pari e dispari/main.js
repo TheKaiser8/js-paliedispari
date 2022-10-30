a50b56e7b9a67088d867d2c0e23613838aedfc83
TheKaiser8 <stefano.vanzini8@gmail.com>
2022-10-30 14:25:38 +0100

    Aggiunge scelta utente (pari o dispari) Include la validazione del dato inserito con un ciclo while

--- pari e dispari/main.js
+++ pari e dispari/main.js
@@ -12,3 +12,20 @@
 /*------------------
     MAIN
 --------------------*/
+// 1. Chiedo all'utente se vuole scommettere su pari o dispari ed effettuo la validazione del dato inserito
+let userChoice = prompt(`Scrivi "pari" o "dispari"`);
+while( !(userChoice === "pari" || userChoice === "dispari") ) {
+    alert(`Devi scrivere "pari" oppure "dispari"!`);
+    userChoice = prompt(`Scrivi "pari" o "dispari"`);
+}
+console.log(`La scelta dell'utente è: "${userChoice}"`);
+
+// 2. Chiedo all'utente di inserire un numero tra 1 e 5 ed effettuo la validazione del dato inserito
+
+// 3. Genero un numero random (sempre da 1 a 5) per il computer (usando una funzione)
+
+// 4. Sommo il numero scelto dall'utente e quello generato per la CPU
+
+// 5. Definisco se la somma dei due numeri è pari o dispari (usando una funzione)
+
+// 6. Dichiaro il vincitore
